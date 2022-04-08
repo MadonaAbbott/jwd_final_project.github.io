4ac0ab73b5ad1ea8fd20b51863becf9468adc88c
Madona Abbott <happy_mikay1985@yahoo.com>
2022-04-08 17:56:51 -0400

    edit index.js

--- main/js-folder/index.js
+++ main/js-folder/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+function validFormFieldInput(data) {
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const name = newTaskNameInput.value;
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const name = newTaskNameInput.value;
+    console.log("name:  " + name);
+
+}
